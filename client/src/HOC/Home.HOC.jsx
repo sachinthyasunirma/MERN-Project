26637ac3744c7aa070197552fccb600ec4a67f3e
sachinthyasunirma <ssrathnavibushana@gmail.com>
2021-10-15 13:23:04 +0530

    Strart FrontEnd- Init React and Tailwind css - init the Layout and HOC

---
+++ client/src/HOC/Home.HOC.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Router } from 'react-router-dom';
+
+//Layout
+import HomeLayout from '../Layout/Home.layout';
+
+function HomeLayoutHOC() {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default HomeLayoutHOC;
